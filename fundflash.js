83d85111f6132b9a43cfc46ef7dce4e2426e9e27
fscaptain5 <monitorsabeel1@gmail.com>
2024-05-31 23:04:25 +0530

    Create fundflash.js

---
+++ fundflash.js
@@ -0,0 +1,8 @@
+        {
+          document.getElementById("gv").innerHTML = "0.12%";
+        }
+        {
+          document.getElementById("sv").innerHTML = "8r";
+        }
+        
+        
